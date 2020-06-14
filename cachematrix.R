9fc64085cfc588ac5b92f96627731d4a22726248
aravindkaruturi <karuturi.aravind@siom.in>
2020-06-14 12:04:55 +0530

    typo error corrected makeCacheMatrix and cacheSolve functons added in the cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -15,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
                            i <<- NULL
                    }
                    get <- function() x
-                   setinverse <- function(imverse) i <<- inverse
+                   setinverse <- function(inverse) i <<- inverse
                    getinverse <- function() i
                    list(set=set,get=get,
                         setinverse=setinverse,getinverse=getinverse)
